cd7f86f9576380dab87ee90e5492913572199bba
Rohit24256 <rohitnar05@gmail.com>
2025-12-10 12:12:46 +0530

    Revision 1: Displayed parent and child process IDs

--- ipc_simulator.cpp
+++ ipc_simulator.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <unistd.h>
 using namespace std;
-
 void simulatePipe() {
     int pipefd[2];
     char buffer[50];
-
     if (pipe(pipefd) == -1) {
         cout << "Pipe creation failed!" << endl;
         return;
     }
-
+    // Display parent PID (Revision 2)
+    cout << "Parent PID: " << getpid() << endl;
     int pid = fork();
-
     if (pid < 0) {
         cout << "Fork failed!" << endl;
         return;
     }
-
     // Child Process
     if (pid == 0) {
         close(pipefd[0]); // Close read end
@@ -33,7 +30,6 @@ void simulatePipe() {
         close(pipefd[0]);
     }
 }
-
 int main() {
     cout << "=== IPC Debugger Tool: Pipe Simulation ===\n";
     simulatePipe();
